e768fe7c6ec78da53d35c5e4246fa3e4cc9ed450
GimHayeong <pinkjoora@gmail.com>
2019-03-06 05:25:08 +0900

    bootstrap 4

---
+++ Scripts/bts.validationDateFormat.js
@@ -0,0 +1,59 @@
+﻿ko.bindingHandlers.date = {
+    init: function (element, valueAccessor, allBindingAccessor, viewModel) {
+        $(element).mask("9999/99/99", { placeholder: "yyyy/mm/dd" });
+        ko.utils.registerEventHandler(element, "change", () => {
+            var value = valueAccessor();
+            if (moment(element.value).isValid()) {
+                value(element.value);
+            } else {
+                value(null);
+            }
+        });
+        ko.validation.makeBindingHandlerValidatable("date");
+    },
+    update: function (element, valueAccessor, allBindingAccessor, viewModel) {
+        var value = valueAccessor();
+        var allBindings = allBindingAccessor();
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+        var pattern = allBindings.format || "yyyy/mm/dd";
+        var output = null;
+
+        if (valueUnwrapped != null && valueUnwrapped != undefined && valueUnwrapped.length > 0) {
+            output = moment(valueUnwrapped).format(pattern);
+        }
+
+        if ($(element).is("input") == true) {
+            $(element).val(output);
+        } else {
+            $(element).text(output);
+        }
+    }
+};
+ko.validation.makeBindingHandlerValidatable("date");
+
+ko.validation.rules["simpleDate"] = {
+    validator: (val, validate) =>{
+        if (val == null) return true;
+        return moment(val).isValid();
+    },
+    message: "Invalid date"
+};
+
+ko.observable($("#ReleaseDate").val());
+
+//시작 날짜 선택후, 종료날짜는 시작날짜 이후 선택하도록 설정
+//initDateTerm("#datepicker1", "#datepicker2");
+function initTerm(startTarget, endTarget) {
+    $(startTarget).datepicker({
+        numberOfMonths: 2,
+        onSelect: function (selected) {
+            $(endTarget).datepicker("option", "minDate", selected)
+        }
+    });
+    $(endTarget).datepicker({
+        numberOfMonths: 2,
+        onSelect: function (selected) {
+            $(startTarget).datepicker("option", "maxDate", selected)
+        }
+    });
+}
---
+++ Scripts/slideshow.js
@@ -0,0 +1,44 @@
+﻿var slideIndex = 1;
+var timeIndex = 0;
+showSlideItem();
+
+// Next/previous controls
+function nextSlideItem(n) {
+    slideIndex += n;
+    slideIndex--;
+
+    if (timeIndex > 0) clearTimeout(timeIndex);
+    showSlideItem();
+}
+
+// Thumbnail image controls
+function currentSlideItem(n) {
+    slideIndex = n;
+
+    if (timeIndex > 0) clearTimeout(timeIndex);
+    showSlideItem();
+}
+
+function showSlideItem() {
+    var i;
+
+    var slides = $(".slideItem");
+    var dots = $(".dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    if (slideIndex < 1) { slideIndex = slides.length; }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    slideIndex++;
+
+    timeIndex = setTimeout(showSlideItem, 3000);
+}
